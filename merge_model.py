6a813c14cab865e1db88599968fad8a607d1507a
Thomas Bellucci <th.bellucci@gmail.com>
2025-12-25 16:19:49 +0100

    Added uv project and uint8 quantization support

--- merge_model.py
+++
@@ -1,8 +0,0 @@
-import onnx
-
-model = onnx.load("checkpoints/da3-small/model.onnx", load_external_data=True)
-onnx.save(
-    model,
-    "checkpoints/da3-small/model_no_external.onnx",
-    save_as_external_data=False
-)
